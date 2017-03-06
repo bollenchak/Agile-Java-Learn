24bfdee55ffdb50de5cc9d1c244e2e93d2b5998b
bollenchak <bollen@aliyun.com>
2017-03-06 21:56:07 +0800

    refactor code

--- 2-Java Basics/AllTests.java
+++
@@ -1,9 +0,0 @@
-public class AllTests {
-
-	public static junit.framework.TestSuite suite() {
-		junit.framework.TestSuite suite = new junit.framework.TestSuite();
-		suite.addTestSuite(StudentTest.class);
-		suite.addTestSuite(CourseSessionTest.class);
-		return suite;
-	}
-}
--- 2-Java Basics/run.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-javac -cp .;"F:\bollen\Agile-Java-Learn\junit-3.8.1.jar" *.java
-if not errorlevel 1 java -cp .;"F:\bollen\Agile-Java-Learn\junit-3.8.1.jar" junit.awtui.TestRunner AllTests
-pause
